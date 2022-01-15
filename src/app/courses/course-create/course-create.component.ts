53ee08fd9d232a9f7207b81367ea2af8b7cd7960
David Newman <davidjndev@gmail.com>
2022-01-15 10:26:01 -0600

    more firestore 8 -> 9

--- src/app/courses/course-create/course-create.component.ts
+++ src/app/courses/course-create/course-create.component.ts
@@ -1,89 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore } from '@angular/fire/firestore';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ClassData, courseCategories, courseLanguages} from 'src/app/shared/class/class';
+import { ClassData, courseCategories, courseLanguages } from 'src/app/shared/class/class';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
-  selector: 'app-course-create',
-  templateUrl: './course-create.component.html',
-  styleUrls: ['./course-create.component.scss'],
+    selector: 'app-course-create',
+    templateUrl: './course-create.component.html',
+    styleUrls: ['./course-create.component.scss'],
 })
 export class CreateCourseComponent implements OnInit {
-  courseName: string = ""
-  languages = courseLanguages
-  categories = courseCategories
-  courseMetadataForm!: FormGroup
-  courseData: ClassData | undefined
+    courseName: string = ""
+    languages = courseLanguages
+    categories = courseCategories
+    courseMetadataForm!: FormGroup
+    courseData: ClassData | undefined
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private afs: AngularFirestore,
-  ) {
-  }
-  classFields = [
-    {field: 'ClassName', display: 'Class Name'},
-    {field: 'CourseNumber', display: 'Course Number'},
-    {field: 'GraphicUrl', display: 'Graphic URL (from course page - url(https://...) )'},
-  ]
+    constructor(
+        private formBuilder: FormBuilder,
+        // private afs: AngularFirestore,
+        private afs: Firestore,
+    ) {
+    }
+    classFields = [
+        { field: 'ClassName', display: 'Class Name' },
+        { field: 'CourseNumber', display: 'Course Number' },
+        { field: 'GraphicUrl', display: 'Graphic URL (from course page - url(https://...) )' },
+    ]
 
-  ngOnInit(): void {
-    this.courseMetadataForm = this.formBuilder.group({
-      ClassName: ['', Validators.required],
-      CourseNumber: ['', Validators.required],
-      DifficultyAvg: [0, Validators.required],
-      DifficultyCount: [0, Validators.required],
-      GraphicUrl: ['url(https://ws.engr.illinois.edu/images/block.i.color.png)', Validators.required],
-      RatingAvg: [0, Validators.required],
-      RatingCount: [0, Validators.required],
-      WorkloadAvg: [0, Validators.required],
-      WorkloadCount: [0, Validators.required],
-      category: ['', Validators.required],
-      languages: ['', Validators.required],               
-      seasonSpring: [false, Validators.required],
-      seasonSummer: [false, Validators.required],
-      seasonFall: [false, Validators.required],
-    })
-  }
-
-  get f() {
-    return this.courseMetadataForm?.controls
-  }
-
-  setFieldData(field: AbstractControl, fieldBool: AbstractControl, fieldValue: any): void {
-    fieldValue ? field.setValue(fieldValue) : ""
-    fieldValue ? fieldBool.setValue("true") : fieldBool.setValue("false")
-    fieldValue ? field.enable() : field.disable()
-    fieldBool.valueChanges.subscribe(val => {val=="true" ? field.enable() : field.disable()})
-  }
-
-  onSubmit(): void {
-    this.afs.collection('Class')
-        // .add(this.courseMetadataForm.value)
-        .add({
-          ClassName: this.f.ClassName.value,
-          CourseNumber: this.f.CourseNumber.value,
-          DifficultyAvg: this.f.DifficultyAvg.value,
-          DifficultyCount: this.f.DifficultyCount.value,
-          GraphicUrl: this.f.GraphicUrl.value,
-          RatingAvg: this.f.RatingAvg.value,
-          RatingCount: this.f.RatingCount.value,
-          WorkloadAvg: this.f.WorkloadAvg.value,
-          WorkloadCount: this.f.WorkloadCount.value,
-          category: [],
-          languages: [],
-          season: {
-            spring: this.f.seasonSpring.value === "true",
-            summer: this.f.seasonSummer.value === "true",
-            fall: this.f.seasonFall.value === "true",
-          },
-          semesters: {},
+    ngOnInit(): void {
+        this.courseMetadataForm = this.formBuilder.group({
+            ClassName: ['', Validators.required],
+            CourseNumber: ['', Validators.required],
+            DifficultyAvg: [0, Validators.required],
+            DifficultyCount: [0, Validators.required],
+            GraphicUrl: ['url(https://ws.engr.illinois.edu/images/block.i.color.png)', Validators.required],
+            RatingAvg: [0, Validators.required],
+            RatingCount: [0, Validators.required],
+            WorkloadAvg: [0, Validators.required],
+            WorkloadCount: [0, Validators.required],
+            category: ['', Validators.required],
+            languages: ['', Validators.required],
+            seasonSpring: [false, Validators.required],
+            seasonSummer: [false, Validators.required],
+            seasonFall: [false, Validators.required],
         })
-        .then(result => {
-          window.alert("Added! Reload the window to add another one.")
-          location.reload()
-        }, error => {
-          console.error("Create Review: Submission failed -", error)
-          window.alert("Error! See the console for more.")
+    }
+
+    get f() {
+        return this.courseMetadataForm?.controls
+    }
+
+    setFieldData(field: AbstractControl, fieldBool: AbstractControl, fieldValue: any): void {
+        fieldValue ? field.setValue(fieldValue) : ""
+        fieldValue ? fieldBool.setValue("true") : fieldBool.setValue("false")
+        fieldValue ? field.enable() : field.disable()
+        fieldBool.valueChanges.subscribe(val => { val == "true" ? field.enable() : field.disable() })
+    }
+
+    async onSubmit() {
+        const ref = collection(this.afs, 'Class')
+        await addDoc(ref, {
+            ClassName: this.f.ClassName.value,
+            CourseNumber: this.f.CourseNumber.value,
+            DifficultyAvg: this.f.DifficultyAvg.value,
+            DifficultyCount: this.f.DifficultyCount.value,
+            GraphicUrl: this.f.GraphicUrl.value,
+            RatingAvg: this.f.RatingAvg.value,
+            RatingCount: this.f.RatingCount.value,
+            WorkloadAvg: this.f.WorkloadAvg.value,
+            WorkloadCount: this.f.WorkloadCount.value,
+            category: [],
+            languages: [],
+            season: {
+                spring: this.f.seasonSpring.value === "true",
+                summer: this.f.seasonSummer.value === "true",
+                fall: this.f.seasonFall.value === "true",
+            },
+            semesters: {},
         })
-  }
+            .then(result => {
+                window.alert("Added! Reload the window to add another one.")
+                location.reload()
+            }, error => {
+                console.error("Create Review: Submission failed -", error)
+                window.alert("Error! See the console for more.")
+            })
+    }
 }
